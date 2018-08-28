af154deae328487617081b7c83e56822098df708
NjeriNjoroge <wanjorogegrace@gmail.com>
2018-08-28 16:08:19 +0300

    implemented Apple Push Notifications

---
+++ APNDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APNDemo
+//
+//  Created by Grace Njoroge on 28/08/2018.
+//  Copyright © 2018 Grace Njoroge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
